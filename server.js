31cdc8a8758293feb0799d33b4038d092ac60f53
Jan Stastny <honza@stastnarodina.com>
2017-12-15 09:25:50 +0100

    Write JSON already formatted

--- server.js
+++ server.js
@@ -3,85 +3,96 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 const bodyParser = require('body-parser');
-const jsonFormat = require('json-format');
-
+const _ = require('lodash/core');
 
 const DATA_DIR = os.tmpdir();
 
 const app = express();
 
-app.use(bodyParser.raw({
-    inflate: true,
-    limit: '100kb',
-    type: 'application/json'
-}));
+// create application/json parser
+const jsonParser = bodyParser.json();
 
 PORT = process.env.PORT || 8877;
-TOKEN = 'bda98695-b53e-4a63-a9c5-8b115ba6539c';
+TOKEN = process.env.TOKEN || 'bda98695-b53e-4a63-a9c5-8b115ba6539c';
 
 app.get(`/${TOKEN}`, (req, res) => res.send('Simple Web Server!'));
 
-app.get('/aaa/:bucket_id', (req, res) => {
-    console.log(req.params);
-    res.send("fff")
+app.use((req, res, next) => {
+    res.set('Content-Type', 'text/plain');
+    next()
 });
 
-app.get(`/${TOKEN}/:bucket_id`, (req, res, next) => {
-        const bucket_dir = path.join(DATA_DIR, req.params['bucket_id']);
+app.use(`/${TOKEN}/:bucket_name`, (req, res, next) => {
+    if (!req.params['bucket_name'].match(/^[A-Za-z0-9_-]+$/)) {
+        console.error("Invalid bucket name: " + req.params['bucket_name']);
+        return res.status(400).send('Invalid bucket name.');
+    }
+    next()
+});
 
-        if (bucket_dir.includes('.')) {
-            return next('Invalid bucket name');
+app.get(`/${TOKEN}/:bucket_name`, (req, res, next) => {
+    const bucket_dir = path.join(DATA_DIR, req.params['bucket_name']);
+
+    if (!fs.existsSync(bucket_dir)) {
+        fs.mkdirSync(bucket_dir);
+    }
+    fs.readdir(bucket_dir, (err, files) => {
+        if (err) return next(err);
+
+        const files_subset = files.sort().reverse().slice(0, 100);
+        if (files_subset.length === 0) {
+            res.write("No requests in this bucket yet. Send POST request to this URL and check again.");
+            res.write("\n");
+            res.write("Sample curl call:");
+            res.write("\n");
+            res.end()
         }
-        res.set('Content-Type', 'text/plain');
 
-        if (!fs.existsSync(bucket_dir)) {
-            fs.mkdirSync(bucket_dir);
-        }
-        fs.readdir(bucket_dir, (err, files) => {
-            if (err) return next(err);
+        files_subset.forEach((filename, idx, array) => {
+            fs.readFile(path.join(bucket_dir, filename), (err, content) => {
+                res.write("============ " + filename + " ============");
+                res.write("\n");
 
-            const files_subset = files.sort().reverse().slice(0, 100);
-            if (files_subset.length === 0) {
-                res.send("No requests in this bucket yet. Send POST request to this URL and check again.")
-            }
+                res.write(content);
+                res.write("\n");
 
-            files_subset.forEach((filename, idx, array) => {
-                fs.readFile(path.join(bucket_dir, filename), (err, content) => {
-                    res.write("============ " + filename + " ============");
-                    res.write("\n");
+                res.write("=======================================================");
+                res.write("\n");
+                res.write("\n");
 
-                    res.write(JSON.stringify(JSON.parse(content), null, '\t'));
-                    res.write("\n");
-
-                    res.write("#######################################################")
-                    res.write("\n");
-                    res.write("\n");
-
-                    if (idx === array.length - 1) {
-                        res.end()
-                    }
-                });
+                if (idx === array.length - 1) {
+                    res.end()
+                }
             });
         });
-    }
-);
+    });
+});
 
-app.post(`/${TOKEN}/:bucket_id`, (req, res, next) => {
-        const bucket_dir = path.join(DATA_DIR, req.params['bucket_id']);
-        if (!fs.existsSync(bucket_dir)) {
-            fs.mkdirSync(bucket_dir);
+app.post(`/${TOKEN}/:bucket_name`, jsonParser, (req, res, next) => {
+    if (_.isEmpty(req.body)) {
+        return res
+            .status(400)
+            .send("Invalid request. Only application/json requests are supported. " +
+                "Make sure, you are setting Content-Type header correctly.");
+    }
+    console.log(req.body);
+    const bucket_dir = path.join(DATA_DIR, req.params['bucket_name']);
+    if (!fs.existsSync(bucket_dir)) {
+        fs.mkdirSync(bucket_dir);
+    }
+    const timestamp = new Date().toISOString();
+    const filename = timestamp + '.json';
+    const file_path = path.join(bucket_dir, filename);
+    const content = JSON.stringify(req.body, null, 2);
+    fs.writeFile(file_path, content, (err) => {
+        if (err) {
+            console.error("Failed to write to file: " + file_path + ". Content: " + content);
+            return next(err)
         }
-        const timestamp = new Date().toISOString();
-        const filename = timestamp + '.json';
-        const file_path = path.join(bucket_dir, filename);
-        fs.writeFile(file_path, req.body, (err) => {
-            if (err) return next(err);
-            res.send('Accepted');
-        });
 
-    }
-);
-
+        res.send('Accepted');
+    });
+});
 
 app.use((req, res, next) =>
     res.status(404).send("Page not found. Maybe invalid token?")
